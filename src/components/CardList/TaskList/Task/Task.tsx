f7cebb03af24692fcfeef3962f1a6e9b039dafdb
Vadim Popov <65708779+senner-code@users.noreply.github.com>
2021-11-05 18:58:10 +0200

    Created Task Menu

--- src/components/CardList/TaskList/Task/Task.tsx
+++ src/components/CardList/TaskList/Task/Task.tsx
@@ -1,8 +1,14 @@
-import React, {FC, useState} from 'react';
+import React, {Dispatch, FC, useEffect, useState} from 'react';
 import TaskController, {TaskI} from "../../../../controller/task.controller";
 import './Task.scss'
 
-const Task: FC<TaskI> = (
+
+interface TaskControl extends TaskI{
+  setTaskMenu : Dispatch<TaskI>
+  menuTask: TaskI | undefined
+}
+
+const Task: FC<TaskControl> = (
   {
     title,
     id,
@@ -10,29 +16,50 @@ const Task: FC<TaskI> = (
     completed,
     percent,
     description,
-    deadline
+    deadline,
+    start,
+    setTaskMenu,
+    menuTask
   }
 ) => {
 
 
-  const [complete, setComplete] = useState<boolean>(completed)
+  useEffect(() => {
+
+    menuTask?.id === id ? setSelected(true) : setSelected(false)
+
+  }, [menuTask])
+
+  const [completedTask, setCompletedTask] = useState<boolean>(completed)
+
+  const [selected, setSelected] = useState<boolean>(false)
 
   return (
     //Сделать функцию по измененению чекед , но с таймаутом с рандомным промежутком 0.5 секунд секунд, при этом в list изменить
-    <div className={'task'} id={`${id}`}>
+    <div className={selected ? 'task task_active' : 'task'} id={`${id}`}>
 
       <div className="task_check">
-        <div className={complete ? "task__checkbox task__checkbox_completed" : "task__checkbox"}
+        <div className={completedTask ? "task__checkbox task__checkbox_completed" : "task__checkbox"}
              onClick={() => {
                TaskController.changeStatus(id, !completed).then(() => {
-                 setComplete(!complete)
+                 setCompletedTask(!completedTask)
                })
              }}
         />
       </div>
 
-      <div className="task__info">
-
+      <div className="task__info" onClick={(e) => {
+        e.stopPropagation()
+        if (menuTask?.id) {
+          if(menuTask?.id !== id){
+            setTaskMenu({id, title, completed: completedTask, deadline, description, card_id, percent, start})
+          }else{
+            setTaskMenu({id:0, title: '', completed: false, deadline: null, description: null, card_id: 0, percent: null, start:  null})
+          }
+        }else {
+          setTaskMenu({id, title, completed:completedTask, deadline, description, card_id, percent, start})
+        }
+      }}>
         <p className="task__title">{title}</p>
         <p className="task__deadline">{deadline}</p>
       </div>
